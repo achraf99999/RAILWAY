8b74b08ed5ea1f9d6ca85282a485c90612f7511e
achraf99999 <achref.lassoued@essai.ucar.tn>
2024-09-28 11:29:37 +0100

    landing pages

--- image_noise_project/settings.py
+++ image_noise_project/settings.py
@@ -65,16 +65,13 @@ INSTALLED_APPS = [
 
 
 
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
-'whitenoise.middleware.WhiteNoiseMiddleware',
-'django. contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',  # Corrected line
-'django.middleware.csrf.CsrfViewMiddleware',
-'django.contrib.auth.middleware.AuthenticationMiddleware',
-'django.contrib.messages.middleware.MessageMiddleware',
-'django.middleware.clickjacking.XFrameOptionsMiddleware',
-'django_htmx.middleware.HtmxMiddleware',
-'django.mid@leware',
+MIDDLEWARE = [    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'image_noise_project.urls'
